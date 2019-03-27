1a9746afb99547229c8a87c2421e582c0cbb91fd
Harry <chanharryhk@gmail.com>
2019-03-27 01:58:30 -0400

    finished most things

--- QTMA.php
+++ QTMA.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link href="indexCSS.css" type="text/css" rel="stylesheet" >
+<link href="schedule.css" type="text/css" rel="stylesheet" >
 </head>
 
 <div class="navi">
@@ -13,36 +13,36 @@
   <li><a href="Hotel.php">Hotel</a></li>
   <li><a href="schedule.php">Schedule</a></li>
   <li><a href="sponsor.php">Sponsor</a></li>
-  <li><a href="conference.php">Conference</a></li>  
+  <li><a href="conference.php">Conference</a></li>
   <li><a href="registration.html">Registration</a></li>
-    <li><a href="totalReg.php">Total Registration</a></li>  
+    <li><a href="totalReg.php">Total Registration</a></li>
 </div>
 </ul>
 </div>
 
 <body>
-<h2>Employee Information</h2>
+<div class="table-container">
+  <h2 class="table-title">Employee Information</h2>
+  <?php
+
+  echo "<table><tr><th>First Name</th><th>Last Name</th></tr>";
+
+  #connect to the database
+  $pdo = new PDO('mysql:host=localhost;dbname=conference2', "root", "");
+
+  $sql = "select fName, lName from committeeMember where commName= 'QTMA'";
+  $stmt = $pdo->prepare($sql);   #create the query
+  $stmt->execute();   #bind the parameters
+
+  #stmt contains the result of the program execution
+  #use fetch to get results row by row.
+  while ($row = $stmt->fetch()) {
+  	echo "<tr><td>".$row["fName"]."</td><td>".$row["lName"]."</td></tr>";
+  }
 
 
-<?php
-
-echo "<table><tr><th>First Name</th><th>Last Name</th></tr>";
-
-#connect to the database
-$pdo = new PDO('mysql:host=localhost;dbname=conference2', "root", "");
-
-$sql = "select fName, lName from committeeMember where commName= 'QTMA'";
-$stmt = $pdo->prepare($sql);   #create the query
-$stmt->execute();   #bind the parameters
-
-#stmt contains the result of the program execution
-#use fetch to get results row by row.
-while ($row = $stmt->fetch()) {
-	echo "<tr><td>".$row["fName"]."</td><td>".$row["lName"]."</td></tr>";
-}
-
-
-?>
-</table>
+  ?>
+  </table>
+</div>
 </body>
-</html> 
+</html>
