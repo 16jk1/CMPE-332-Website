b5382590ece237931148fbeefe7153f26925b4c4
Harry <chanharryhk@gmail.com>
2019-03-27 00:22:27 -0400

    tables basically complete

--- scheduleView.php
+++ scheduleView.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link href="indexCSS.css" type="text/css" rel="stylesheet" >
+<link href="schedule.css" type="text/css" rel="stylesheet" >
 </head>
 
 <div class="navi">
@@ -14,8 +14,8 @@
   <li><a href="schedule.php">Schedule</a></li>
   <li><a href="sponsor.php">Sponsor</a></li>
   <li><a href="conference.php">Conference</a></li>
-  <li><a href="registration.html">Registration</a></li>  
-    <li><a href="totalReg.php">Total Registration</a></li>  
+  <li><a href="registration.html">Registration</a></li>
+    <li><a href="totalReg.php">Total Registration</a></li>
 </div>
 </ul>
 </div>
@@ -36,34 +36,35 @@
 <br>
 <input type="submit" name="submit" value="See Selected Values"/>
 </form>
+<div class="table-container">
+  <?php
 
-<?php
+  $date = $_POST["date"];
 
-$date = $_POST["date"];
-	
-echo "<h2>$date</h2>";
-echo "<table><tr><th>Session Name</th><th>Start Time</th><th>End Time</th><th>Room Location</th><th>Date</th></tr>";
+  echo "<h2 class="table-title">$date</h2>";
+  echo "<table><tr><th>Session Name</th><th>Start Time</th><th>End Time</th><th>Room Location</th><th>Date</th></tr>";
 
-#connect to the database
-$pdo = new PDO('mysql:host=localhost;dbname=conference2', "root", "");
-If($date != "All Events")
-	$sql = "select * FROM session where date = '$date'";
-else
-	$sql = "select * FROM session";
-$stmt = $pdo->prepare($sql);   #create the query
-$stmt->execute();   #bind the parameters
+  #connect to the database
+  $pdo = new PDO('mysql:host=localhost;dbname=conference2', "root", "");
+  If($date != "All Events")
+  	$sql = "select * FROM session where date = '$date'";
+  else
+  	$sql = "select * FROM session";
+  $stmt = $pdo->prepare($sql);   #create the query
+  $stmt->execute();   #bind the parameters
 
-#stmt contains the result of the program execution
-#use fetch to get results row by row.
-while ($row = $stmt->fetch()) {
-	echo "<tr><td>".$row["sessionName"]."</td><td>".$row["startTime"]."</td><td>".$row["endTime"]."</td><td>".$row["roomLocation"]."</td><td>".$row["date"]."</td></tr>";
-}
+  #stmt contains the result of the program execution
+  #use fetch to get results row by row.
+  while ($row = $stmt->fetch()) {
+  	echo "<tr><td>".$row["sessionName"]."</td><td>".$row["startTime"]."</td><td>".$row["endTime"]."</td><td>".$row["roomLocation"]."</td><td>".$row["date"]."</td></tr>";
+  }
 
 
-?>
-</table>
+  ?>
+  </table>
+</div>
 
 <h2><a href="changescheduletime.php">Change Schedule</a></h2>
 
 </body>
-</html> 
+</html>
